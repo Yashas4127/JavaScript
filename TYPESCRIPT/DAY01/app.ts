ce875652b8e697f3cd232f6e9a4211d60ab2652e
Yashas H M <yashashm19@gmail.com>
2025-11-05 05:23:45 +0530

    Ts

---
+++ TYPESCRIPT/DAY01/app.ts
@@ -0,0 +1,18 @@
+//Number
+let a:number=10
+
+//String
+let b:string="Yashas"
+
+//Boolean
+let c:boolean=true;
+
+//Bigint
+let d:bigint= 564946960020402n
+
+//null 
+let e:null=null
+
+//undefined
+let f:undefined=undefined
+
