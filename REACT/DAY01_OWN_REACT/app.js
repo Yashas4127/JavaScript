99fbfc9d0b4b43d4f97dde248411797d3811f4a1
Yashas H M <yashashm19@gmail.com>
2025-11-07 05:31:36 +0530

    hi

---
+++ REACT/DAY01_OWN_REACT/app.js
@@ -0,0 +1,8 @@
+const element = React.createElement("h1",{},"Hello Coder Army");
+
+// ReactDOM.render(element,document.getElementById('root')); only in react 17 and below
+// element : 50 card pade hue hai
+
+const Reactroot = ReactDOM.createRoot(document.getElementById('root'));
+// React root container: is
+Reactroot.render(element);
